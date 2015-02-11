0db8ed9a6a3cd27bb023b06ab39c35fcf6162367
J-Weeks <weeksjasons@gmail.com>
2015-02-10 22:37:12 -0500

    added district and pol trial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+District.delete_all
+Pol.delete_all
+district_one = District.create!(name: "District One", zipcode: "02139")
+pol_one = Pol.create!(party: "Democrat", name: "Bob", district: district_one)
