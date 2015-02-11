32cc5c44e8e2f74071ffb4ff70721b48b2fc4b7e
J-Weeks <weeksjasons@gmail.com>
2015-02-10 22:54:12 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
-District.delete_all
+Vote.delete_all
+Bill.delete_all
 Pol.delete_all
+District.delete_all
 district_one = District.create!(name: "District One", zipcode: "02139")
 pol_one = Pol.create!(party: "Democrat", name: "Bob", district: district_one)
+
+1.upto(10) do |i|
+  bill = Bill.create!(name:"Bill#{i}", number: "#{i}", status: "status #{i}")
+  Vote.create!(how_vote: "Yea", pol: pol_one, bill: bill)
+end
+
