a3d89f33375e06c603d7cb74aa7d072d39a671af
J-Weeks <weeksjasons@gmail.com>
2015-02-10 22:21:35 -0500

    first commit

---
+++ app/models/district.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: districts
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  zipcode    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class District < ActiveRecord::Base
+end
